5226f46dc4250e8f7ff27d9143b8b96078dba809
Amardeep Saha <amardeepsaha13@gmail.com>
2022-07-05 17:50:40 +0530

    new"

---
+++ src/pages/mainpage.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Contact from "./contact";
+import Experience from "./experience";
+import { HashRouter } from "react-router-dom";
+import Home from "./home";
+import Projects from "./projects";
+import { HashLink } from "react-router-hash-link";
+import logo from '../My project.jpg';
+import links from '../utility.json'
+
+export default function Main(){
+
+    
+    const Lnk=links.links;
+    return (
+        <div
+        style={{
+            display:"flex",
+            flexDirection:"column",
+            alignItems:"center",
+            justifyContents:"center",
+            width:"100vw",
+            height:"auto",
+            position:"absolute",
+        }}>
+            <div id="header" style={{
+                justifyContent:"center",
+                display:"flex",
+                flexDirection:"row",
+                alignItems:"center",
+                margin:"auto",
+                padding:"15px", 
+                height:"87vh",  
+                position:"sticky",
+                fontFamily:"'Balsamiq Sans', cursive",
+                fontSize:"10vh",
+                fontSmooth:"always",
+            }}>
+                <div style={{display:"flex",
+                flexDirection:"row",margin:"auto",}}>
+                <div style={{color:"purple"}}>Amardeep&nbsp;</div>
+                <div >Saha</div>
+                </div>
+                <div style={{
+                    display:"flex",
+                    alignItems:"center",
+                    justifyContent:"center",
+                    marginLeft:"10vh",
+                    borderRadius:"50%",
+                    height:"30vh",
+                    width:"30vh",
+                    borderBlockColor:"purple",
+                    borderBlockStyle:"solid",
+                    borderBlockWidth:"1vh",
+                }}>
+                    <img src={logo} style={{height:"30vh",width:"30vh",borderRadius:"50%"}} alt="Amardeep Saha"/>
+                </div>
+            </div>
+            <div style={{ display:"flex",
+            flexDirection:"row",
+            height:"auto",
+            width:"100vw",
+            margin:"auto",
+            justifyContent:"center",
+            alignItems:"center",
+            position:"sticky",
+            top:"0px",
+            backgroundColor:"white",
+            boxShadow:"initial"
+        }}>
+            
+            <BrowserRouter>
+            {Lnk.map((nm)=>
+                {
+                    return(<div style={{
+                        width:"auto",
+                        height:"auto",
+                        margin:"auto",
+                        padding:"3vh",
+                        width:"auto",
+                    }}
+                ><HashLink to={nm.link} smooth style={{ textDecoration:"none"}}>{nm.name}</HashLink></div>
+                )
+                }
+            )}
+            </BrowserRouter>          
+        </div>            
+            <Home></Home>
+            <Projects></Projects>
+            <Experience></Experience>
+            <Contact></Contact>
+        </div>
+    )
+}
