34bdf23e7fe934e9352ab063a16946f638c5ee09
alpha-coder13 <56953772+alpha-coder13@users.noreply.github.com>
2022-07-07 20:52:43 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -22,7 +22,7 @@ export default function Home(){
             margin:"auto",
             height:"auto"
         }}>
-        <div style={{display:"flex",justifyContent:"flex-start",alignItems:"center",fontFamily:"'Balsamiq Sans',cursive",fontSize:"5vh",margin:"auto",}}>
+        <div style={{display:"flex",justifyContent:"flex-start",alignItems:"center",fontFamily:"'Balsamiq Sans',cursive",fontSize:"5vh"}}>
             About Me   
         </div>
          <div style={{
@@ -36,7 +36,7 @@ export default function Home(){
             margin:"auto",
             height:"auto"
         }}>
-             <img src={{}} style={{height:"30vh",width:"30vh",borderRadius:"50%", margin:"auto",padding:"1vh"}}></img>
+             <img src={{}} style={{height:"30vh",width:"30vh",borderRadius:"50%", marginRight:"3vh",padding:"1vh"}}></img>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et finibus velit, rutrum gravida diam. Nunc ut dolor quis lacus ultricies accumsan et in mi. Morbi nisl ipsum, facilisis id bibendum ac, consequat at arcu. Mauris id vestibulum velit. Vestibulum gravida congue quam. Praesent eu ultricies leo. Morbi ornare justo a lacus pharetra pellentesque. Nam molestie tincidunt odio. Vivamus a pellentesque elit, iaculis mattis felis.
         <br/><br/>
         Nullam ipsum magna, dictum vel orci ac, gravida molestie metus. Integer aliquet sem at ante lobortis tristique. Sed orci ipsum, ullamcorper eu gravida eu, fringilla a ex. Curabitur vehicula quis nisl nec viverra. Maecenas quis quam blandit, tempus velit ut, auctor lacus. Nam a nisl quis augue feugiat auctor quis et orci. Aliquam erat volutpat. Integer mollis lacus quis iaculis malesuada. In condimentum metus sit amet egestas faucibus. </div> 
