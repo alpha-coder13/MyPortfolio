992650d1ede10e49ab7e5eba982289e376c52c66
alpha-coder13 <56953772+alpha-coder13@users.noreply.github.com>
2022-07-07 20:55:17 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -14,7 +14,7 @@ export default function Home(){
         <div style={{
             backgroundColor:"rgba(255,255,255,0.44)",
             borderRadius:"3%",
-            width:"70vw",
+            width:"50vw",
             display:"flex",
             flexDirection:"column",
 
@@ -28,15 +28,16 @@ export default function Home(){
          <div style={{
             backgroundColor:"rgba(255,255,255,0.44)",
             borderRadius:"3%",
-            width:"70vw",
+            width:"50vw",
             display:"flex",
             flexDirection:"row",
 
             padding:"3vh",
             margin:"auto",
-            height:"auto"
+            height:"auto",
+            flexWrap:"wrap
         }}>
-             <img src={{}} style={{height:"30vh",width:"30vh",borderRadius:"50%", marginRight:"3vh",padding:"1vh"}}></img>
+             <img src={{}} style={{height:"30vh",width:"30vh",borderRadius:"50%", marginRight:"3vh"}}></img>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et finibus velit, rutrum gravida diam. Nunc ut dolor quis lacus ultricies accumsan et in mi. Morbi nisl ipsum, facilisis id bibendum ac, consequat at arcu. Mauris id vestibulum velit. Vestibulum gravida congue quam. Praesent eu ultricies leo. Morbi ornare justo a lacus pharetra pellentesque. Nam molestie tincidunt odio. Vivamus a pellentesque elit, iaculis mattis felis.
         <br/><br/>
         Nullam ipsum magna, dictum vel orci ac, gravida molestie metus. Integer aliquet sem at ante lobortis tristique. Sed orci ipsum, ullamcorper eu gravida eu, fringilla a ex. Curabitur vehicula quis nisl nec viverra. Maecenas quis quam blandit, tempus velit ut, auctor lacus. Nam a nisl quis augue feugiat auctor quis et orci. Aliquam erat volutpat. Integer mollis lacus quis iaculis malesuada. In condimentum metus sit amet egestas faucibus. </div> 
