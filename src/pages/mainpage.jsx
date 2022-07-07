033157c555eedf824ea144e819f3b0c467901052
alpha-coder13 <56953772+alpha-coder13@users.noreply.github.com>
2022-07-07 20:23:05 +0530

    Update mainpage.jsx

--- src/pages/mainpage.jsx
+++ src/pages/mainpage.jsx
@@ -43,7 +43,7 @@ export default function Main(){
                 <div style={{display:"flex",
                 flexDirection:"row",alignItems:"flex-end",margin:"auto", fontFamily:"'Balsamiq Sans', cursive",height:"auto"}}>
                 <div style={{fontSize:"3vh",
-                fontSmooth:"always",paddingBottom:"1vh"}}>I'm&nbsp;&nbsp;</div>
+                fontSmooth:"always",paddingBottom:"2vh"}}>I'm&nbsp;&nbsp;</div>
                 <div style={{color:"purple", fontSize:"8vh",
                 fontSmooth:"always"}}>Amardeep&nbsp;</div>  
                 <div style={{ fontSize:"8vh",
