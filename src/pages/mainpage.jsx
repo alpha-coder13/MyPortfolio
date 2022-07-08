d6bbbb9529073c80fd5d4f9e0424b0b482d3017f
alpha-coder13 <56953772+alpha-coder13@users.noreply.github.com>
2022-07-08 19:00:48 +0530

    Update mainpage.jsx

--- src/pages/mainpage.jsx
+++ src/pages/mainpage.jsx
@@ -100,7 +100,7 @@ export default function Main(){
             alignItems:"center",
             position:"sticky",
             top:"0px",
-            backgroundColor:"rgba(255,255,255,0.94)",
+            backgroundColor:"rgba(255,255,255,0.34)",
             backdropFilter:"blur(10px)",
             boxShadow:"-0.2px 5px 7px rgba(0,0,0,0.07)"
         }}>
