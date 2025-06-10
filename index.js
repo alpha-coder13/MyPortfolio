e61e6bd0eaf627466cc33ec861d17be028d72a29
Amardeep Saha <amardeep.saha@indiamart.com>
2025-06-10 10:46:20 +0530

    ....

---
+++ index.js
@@ -0,0 +1,122 @@
+const http =require('http');
+
+const server = http.createServer();
+
+server.listen(3000);
+
+server.on('request',(req,res)=>{
+    return RequestHandler(req,res);
+});
+
+
+function RequestHandler(req,res){
+    const {readFile} = require('fs/promises');
+    const {join} = require('path');
+    console.log(req.url);
+    switch(req.url){
+        case '/':{
+            const filePath =join(__dirname,'data','About.json');
+            const data = readFile(filePath,'utf-8');
+            data.then(data => {
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({
+                    data : data,
+                    error : null,
+                    paths : {
+                        '/':"about me",
+                        '/experience':"my experience",
+                        '/projects':"my projects"
+                    }
+                }));
+                return;
+            }).catch(err => {
+                res.writeHead(404,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({
+                    data : "Not Found",
+                    error : "file not found",
+                    paths : {
+                        '/':"about me",
+                        '/experience':"my experience",
+                        '/projects':"myprojects"
+                    }
+                }));
+                return;
+            })
+            return ;
+        }
+        case '/experience':{
+            const filePath =join(__dirname,'data','Experience.json');
+            const data = readFile(filePath,'utf-8');
+            data.then(data => {
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({
+                    data : JSON.parse(data),
+                    error : null,
+                    paths : {
+                        '/':"about me",
+                        '/experience':"my experience",
+                        '/projects':"my projects"
+                    }
+                }));
+                return;
+            }).catch(err => {
+                res.writeHead(404,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({
+                    data : "Not Found",
+                    error : "file not found",
+                    paths : {
+                        '/':"about me",
+                        '/experience':"my experience",
+                        '/projects':"myprojects"
+                    }
+                }));
+                return;
+            })
+            return ;
+        }
+        case '/projects':{
+            const filePath =join(__dirname,'data','Project.json');
+            const data = readFile(filePath,'utf-8');
+            data.then(data => {
+                console.log(data);
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({
+                    data : JSON.parse(data),
+                    error : null,
+                    paths : {
+                        '/':"about me",
+                        '/experience':"my experience",
+                        '/projects':"my projects"
+                    }
+                }));
+                return;
+            }).catch(err => {
+                res.writeHead(404,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({
+                    data : "Not Found",
+                    error : "file not found",
+                    paths : {
+                        '/':"about me",
+                        '/experience':"my experience",
+                        '/projects':"myprojects"
+                    }
+                }));
+                return;
+            })
+            return ;
+        }
+        default : {
+                res.writeHead(412,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({
+                data : "Not Found",
+                error : "invalid request",
+                paths : {
+                    '/':"about me",
+                    '/experience':"my experience",
+                    '/projects':"my projects"
+                }
+            }));
+            return;
+        }
+    }
+}
