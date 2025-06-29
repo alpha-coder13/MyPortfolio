fc2390ce32e3f914b5b85bba480b19d2f727c199
alpha-coder13 <amardeepsaha13@gmail.com>
2025-06-29 17:17:14 +0530

    debouncing requests

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const debounceFunction  = (fn) => {
         if(called){
             clearTimeout(called);
         }
-        called = setTimeout(()=>fn.apply(this,...args), 1200);
+        called = setTimeout(()=>fn.apply(this,...args), 800);
     }
 }
 
