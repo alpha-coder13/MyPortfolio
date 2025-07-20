d94c0d326f3c2dd1beefc9116a51850238869545
alpha-coder13 <amardeepsaha13@gmail.com>
2025-07-20 15:58:00 +0530

    change in portfolio projects

---
+++ Router/server_webRTC/index.js
@@ -0,0 +1,29 @@
+// const nodeRTC = require('node-webrtc');
+const {Hmac, createHmac} = require('crypto');
+
+function getICEservers(username_input){
+    const username = (Date.now() + (60*60*1000) ) + ':'+username_input;
+    const password = createHmac('sha1',process.env.TURN_STATIC_AUTH_KEY).update(username).digest('base64');
+
+    const iceServer = {
+        username:username,
+        credential:password,
+        urls:[
+            'turn:'+process.env.TURN_IP+':10000?transport=tcp',
+            'turn:'+process.env.TURN_IP+':11000?transport=udp',
+        ]
+    }
+
+    return {"iceServers": [iceServer]}
+}
+function socketRequestHandler(socket){
+        socket.on('message console.log',console.log);
+        socket.on('getServer',(data)=>{
+            let username = data;
+            let iceServers = getICEservers(username); 
+            socket.emit('detail',JSON.stringify(iceServers));
+        })
+}
+
+
+module.exports = {socketRequestHandler};
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const http =require('http');
 const path = require('path');
+
+const {Server} = require('socket.io');
 // require('process').loadEnvFile(path.join(process.cwd(),'.env'));
 require('dotenv').config({path:path.join(process.cwd(),'.env')});
 
 const server = http.createServer();
 // const server_messages = http.createServer();
-
+const ioServer = new Server(server,{
+    cors:{
+        methods:['GET','POST']
+    }
+});
 
 const setCorsHeaders = (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://alpha-coder13.github.io');
+    res.setHeader('Access-Control-Allow-Origin', `https://localhost:${process.env.PORT_GET}`);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   }
@@ -19,11 +25,13 @@ const setCorsHeaders = (req, res) => {
 //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
 // }
 
-server.listen(process.env.PORT,()=>{
-    console.log(process.env.PORT_GET);
-});
 
 
+ioServer.on("connection",(socket)=>{
+    require('./Router/server_webRTC/index').socketRequestHandler(socket); 
+})
+
+ioServer.on('close',console.log);
 
 server.on('request',(req,res)=>{
     setCorsHeaders(req,res);
@@ -34,12 +42,18 @@ server.on('request',(req,res)=>{
     }
     if(req.url.indexOf("sendMessage")==-1){
         require('./Router/server_assets/index').RequestHandler(req,res);
-    }else{
-        require('./Router/server_messages/index').RequestHandler(req,res);
+        return;
     }
-    return;
+    else{
+        require('./Router/server_messages/index').RequestHandler(req,res);
+        return;
+    }
 });
 
+
+server.listen(process.env.PORT_GET,()=>{
+    console.log(process.env.PORT_GET);
+});
 // server_messages.on('request',(req,res)=>{
 //     setCorsHeaders_messages(req,res);
 // })
